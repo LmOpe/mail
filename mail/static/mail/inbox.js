fcedf9ca9322af6f02c08f6c92a74b445a02c649
LmOpe <lawalmuhammed44@gmail.com>
2023-03-04 22:37:47 +0100

    Implement View-mail, such that whenever a user clicks on a maiail it shows the content of the mail only and sets the mail to read

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -27,6 +27,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#view-mail').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -39,18 +40,19 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#view-mail').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   // When user visits their mailbox, populate the mialbox with the appropriate mails
-  if (mailbox == 'inbox'){
+  if (mailbox == 'inbox') {
     populate('inbox');
   }
-  else if(mailbox == 'sent'){
+  else if (mailbox == 'sent') {
     populate('sent');
   }
-  else if(mailbox == 'archive'){
+  else if (mailbox == 'archive') {
     populate('archive');
   }
 }
@@ -68,23 +70,23 @@ function send_mail(recipients, subject, body) {
   })
     .then(response => response.json())
     .then(result => {
-      
+
       // Respond with error or success
       error = result.error;
       success = result.message;
-      if (success != null){
+      if (success != null) {
         document.querySelector('#response').innerHTML = success;
         load_mailbox('sent')
       }
-      else{
+      else {
         document.querySelector('#response').innerHTML = error;
       }
     });
 }
 
 // send get request to server and use the response appropriately
-function populate(mailbox){
-    fetch(`/emails/${mailbox}`)
+function populate(mailbox) {
+  fetch(`/emails/${mailbox}`)
     .then(response => response.json())
     .then(emails => {
       console.log(emails);
@@ -97,15 +99,62 @@ function populate(mailbox){
         subject.innerHTML = email.subject;
         timestamp.innerHTML = email.timestamp;
         element.append(sender, subject, timestamp);
-        if (email.read){
+        if (email.read) {
           element.style.background = 'gray';
         }
-        else{
+        else {
           element.style.background = 'white';
         }
+        element.onclick = () => {
+          fetch(`/emails/${email.id}`)
+            .then(response => response.json())
+            .then(emails => {
+              console.log(emails);
+              viewEmail(emails);
+
+              // Update the mail to read
+              if (email.read == false){
+                fetch(`/emails/${email.id}`, {
+                  method: 'PUT',
+                  body: JSON.stringify({
+                    read: 'True'
+                  })
+                })
+              }
+            })
+        };
         console.log(element);
         document.querySelector('#emails-view').append(element);
       }
       )
     });
+}
+
+// Creates an element that displays the mail information
+function viewEmail(emails) {
+  const sender = document.createElement('p')
+  sender.innerHTML = `<b>From: </b> ${emails.sender}`;
+  const recipients = document.createElement('p');
+  recipients.innerHTML = `<b>To: </b> ${emails.recipients}`;
+  const subject = document.createElement('p');
+  subject.innerHTML = `<b>Subject: </b> ${emails.subject}`;
+  const timestamp = document.createElement('p');
+  timestamp.innerHTML = `<b>Timestamp: </b> ${emails.timestamp}`;
+  const body = document.createElement('p');
+  body.innerHTML = emails.body;
+  const head = document.createElement('div');
+  head.append(sender, recipients, subject, timestamp);
+  const content = document.createElement('div');
+  content.append(body);
+  hr = document.createElement('hr');
+
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#view-mail').style.display = 'block';
+  if (document.querySelector('#view-mail').childElementCount === 0){
+    document.querySelector('#view-mail').append(head, hr, content);
+  }
+  else{
+    document.querySelector('#view-mail').replaceChildren(head, hr, content);
+  }
 }
