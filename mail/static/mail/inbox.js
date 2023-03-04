66b0a9664a55e731a01703a355beacdc2afd15ed
LmOpe <lawalmuhammed44@gmail.com>
2023-03-04 17:49:18 +0100

    Implement ability to send mail

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
@@ -6,8 +6,20 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
-  // By default, load the inbox
+  // // By default, load the inbox
   load_mailbox('inbox');
+
+  // Send mail on submit
+  document.querySelector('#compose-form').onsubmit = () => {
+    const recipients = document.querySelector('#compose-recipients').value;
+    const subject = document.querySelector('#compose-subject').value;
+    const body = document.querySelector('#compose-body').value;
+
+    // send_mail(recipients, subject, body);
+    send_mail(recipients, subject, body);
+    return false;
+  }
+
 });
 
 function compose_email() {
@@ -23,11 +35,39 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+
+// // Create function to send mail to server
+function send_mail(recipients, subject, body) {
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: recipients,
+      subject: subject,
+      body: body
+    })
+  })
+    .then(response => response.json())
+    .then(result => {
+      
+      // Print result
+      error = result.error;
+      success = result.message;
+      if (success != null){
+        document.querySelector('#response').innerHTML = success;
+        load_mailbox('sent')
+      }
+      else{
+        document.querySelector('#response').innerHTML = error;
+      }
+      
+    });
 }
